59144b4aef1d75453d7a0cccfcc200b408d56c7d
kodaki <kodakicodes@gmail.com>
2025-10-28 15:05:14 +0530

    frontend added

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const apiRoutes = require('./routes/api-routes'); 
+const cors = require('cors');
 
 const app = express();
 const PORT = 5001;
 
 app.use(express.json());
-
+app.use(cors());
 mongoose.connect(process.env.MONGO_URI)
     .then(()=> console.log("database connected successfully!"))
     .catch(err => console.error('connection error', err));
